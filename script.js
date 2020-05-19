8a4b7fcca56bf6ce1f23962036c887a9f94de585
AeonGenesis <exveras@hotmail.com>
2020-05-19 14:04:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
+const randBtn = document.getElementById("random");
 
 setGradient();
 
@@ -16,6 +17,19 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function setRandomGradient() {
+    color1.value = randomColor();
+    color2.value = randomColor();
+    setGradient();
+}
+
+function randomColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+console.log(randomColor());
+
 color1.addEventListener("input", setGradient); 
     
 color2.addEventListener("input", setGradient);
+
+randBtn.addEventListener("click", setRandomGradient);
